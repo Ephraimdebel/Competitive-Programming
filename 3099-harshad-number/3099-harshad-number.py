65c9811749d0f3d90adba6156c94ce48632b127c
Ephraimdebel <162354912+Ephraimdebel@users.noreply.github.com>
2024-07-06 09:27:46 -0700

    Time: 33 ms (68.07%), Space: 16.6 MB (29.33%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.py
+++ 3099-harshad-number/3099-harshad-number.py
@@ -1,14 +1,21 @@
 class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
-        sum = 0
-        org = x
-        while x!=0:
-            y = x%10
-            sum+=y
-            x//=10
-        if org%sum==0:
+        # sum = 0
+        # org = x
+        # while x!=0:
+        #     y = x%10
+        #     sum+=y
+        #     x//=10
+        # if org%sum==0:
+        #     return sum
+        # else:
+        #     return -1
+        sum=0
+        for i in str(x):
+            sum += int(i)
+        if x%sum==0:
             return sum
-        else:
+        else: 
             return -1
 
         

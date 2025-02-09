fc4c5f20b1260ef0e9f1f53616b187d933f7455e
Ephraim Debel <162354912+Ephraimdebel@users.noreply.github.com>
2025-02-09 19:20:58 +0300

    Time: 93 ms (38.09%), Space: 38.9 MB (59.29%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        good_pairs = 0
+        total_pairs = 0
+        for i in range(len(nums)):
+            total_pairs+=i
+            good_pairs += count[nums[i] - i]
+            count[nums[i] - i]+=1
+        return total_pairs - good_pairs

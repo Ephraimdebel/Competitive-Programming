fa3aa25efef6bf1c7f567b962f8bda28825aab9d
Ephraim Debel <162354912+Ephraimdebel@users.noreply.github.com>
2025-03-05 23:04:04 +0300

    Time: 0 ms (100%), Space: 17.9 MB (66.83%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for dir in logs:
+            if dir == "../":
+                if count== 0:
+                    continue
+                else:
+                    count-=1
+            elif dir == "./":
+                continue
+            else:
+                count+=1
+        return count

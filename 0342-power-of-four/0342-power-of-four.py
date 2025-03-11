20c4246e2b1e91da86c031c5c9f0765fddeca923
Ephraim Debel <162354912+Ephraimdebel@users.noreply.github.com>
2025-03-11 11:15:57 +0300

    Time: 47 ms (2.78%), Space: 18 MB (20.8%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -5,7 +5,7 @@ class Solution:
         def power(n):
             if n == 1:
                 return True
-            elif n%4 != 0 or n == 0:
+            elif n <= 0:
                 return False
-            return power(n//4)
+            return power(n/4)
         return power(n)
